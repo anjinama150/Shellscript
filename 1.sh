6982d4176b215620acd363c6ffd2e05808f56c5f
sushbsush <sushmithasuhu2@gmail.com>
2022-11-17 17:12:35 +0000

    Sushcommit

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Enter the first int value"
+echo "Enter the first number1 value"
 read a
-echo "Enter the 2nd integer"
+echo "Enter the 2nd number2"
 read b
-echo "First integer is $a"
-echo "The second integer is $b"
+echo "First integer1 is $a"
+echo "The second integer2 is $b"
