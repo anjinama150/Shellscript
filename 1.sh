56cb223ca84ecd0d767b7d1b951c40c9abada593
sushbsush <sushmithasuhu2@gmail.com>
2022-11-17 17:16:11 +0000

    sushedit

--- 1.sh
+++ 1.sh
@@ -3,5 +3,5 @@ echo "Enter the first number1 value"
 read a
 echo "Enter the 2nd number2"
 read b
-echo "First integer1 is $a"
-echo "The second integer2 is $b"
+echo "First number1 is $a"
+echo "The second number2 is $b"
