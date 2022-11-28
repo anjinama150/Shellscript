f65c5850f2b596a2bdc7b5e7e4788febbb3c53d1
anjinama150 <118283202+anjinama150@users.noreply.github.com>
2022-11-28 23:57:37 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-echo "Enter the first number1 value"
-read a
-echo "Enter the 2nd number2"
-read b
-echo "First number1 is $a"
-echo "The second number2 is $b"
 echo "Enter the first int value  to print"
 a=$1
 echo "Enter the 2nd integer to print"
 b=$2
 echo "First integer  number  is $a"
 echo "The second integer number is $b"
-echo anju
 echo triggered from webhook jenkins
 echo jenkins automatic
